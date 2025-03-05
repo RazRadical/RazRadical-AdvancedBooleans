06737f7b879ce370a2ba1e92b14a8d8c75811fcc
RazaDon Willrich <razradical93@gmail.com>
2025-03-05 09:00:19 +0000

    Completed Advanced Booleans.

--- src/main/java/AdvancedBooleanLogic.java
+++ src/main/java/AdvancedBooleanLogic.java
@@ -16,7 +16,8 @@ public class AdvancedBooleanLogic {
      * @return false if b1 and b2 are both true. Otherwise, return true.
      */
     public boolean returnNotBoth(boolean b1, boolean b2){
-        return false;
+        boolean both = !(b1 == true && b2 == true);
+        return both;
     }
     /**
      * b1 OR b2, BUT NOT BOTH
@@ -25,7 +26,8 @@ public class AdvancedBooleanLogic {
      * @return true if either b1 or b2 is true, but false if they are both true or both false.
      */
     public boolean returnExclusiveOr(boolean b1, boolean b2){
-        return false;
+        boolean both = !(b1 == true && b2 == true || b1 == false && b2 == false);
+        return both;
     }
 
     /**
@@ -35,6 +37,7 @@ public class AdvancedBooleanLogic {
      * @return true if neither b1 nor b2 are true. Return false if either b1 or b2 is true.
      */
     public boolean returnNeitherNor(boolean b1, boolean b2){
-        return false;
+        boolean both = (b1 == false && b2 == false);
+        return both;
     }
 }
